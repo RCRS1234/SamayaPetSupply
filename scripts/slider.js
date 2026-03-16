39b8497f5643e9b4edd7c9c2ddeb5c533c24ed92
RCRS1234 <rcrsalimbagat@mymail.mapua.edu.ph>
2026-03-16 17:01:54 +0800

    Update and rename script.js to slider.js

---
+++ scripts/slider.js
@@ -0,0 +1,32 @@
+var images = [
+"images/store.jpg",
+"images/petfood.jpg",
+"images/pets.jpg"
+];
+
+var i = 0;
+
+function slideShow(){
+
+document.getElementById("slide").src = images[i];
+
+i++;
+
+if(i >= images.length){
+i = 0;
+}
+
+setTimeout(slideShow,3000);
+
+}
+
+window.onload = slideShow;
+
+function openPopup(img){
+document.getElementById("imagePopup").style.display = "block";
+document.getElementById("popupImage").src = img.src;
+}
+
+function closePopup(){
+document.getElementById("imagePopup").style.display = "none";
+}
